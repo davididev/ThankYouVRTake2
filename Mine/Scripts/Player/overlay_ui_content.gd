9a440236c66b4a57a845db98a2ff9131a576715a
davididev <davidwaggener@gmail.com>
2024-12-13 23:44:26 -0500

    Added embed ui

---
+++ Mine/Scripts/Player/overlay_ui_content.gd
@@ -0,0 +1,34 @@
+extends VBoxContainer
+
+var refresh_time_timer = 0.0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	refresh_time_timer = 0.0;
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	RefreshTime(delta);
+	
+func RefreshTime(delta: float):
+	refresh_time_timer -= delta;
+	if refresh_time_timer <= 0.0:
+		var today = Time.get_datetime_dict_from_system();
+		var hour = today.hour;
+		if hour >= 12:  #More than 12 PM
+			hour -= 12;
+		var is_pm = today.hour > 12;
+		var minuteStr = str("%10" % today.minute);
+		
+		if hour == 0:  #12 PM or 12 AM
+			if is_pm:
+				get_node("Time Label").text = str(12, ":", minuteStr, "PM");
+			else:
+				get_node("Time Label").text = str(12, ":", minuteStr, "AM");
+		else:
+			if is_pm:
+				get_node("Time Label").text = str(hour, ":", minuteStr, "PM");
+			else:
+				get_node("Time Label").text = str(hour, ":", minuteStr, "AM");
