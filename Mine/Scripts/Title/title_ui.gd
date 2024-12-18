e49a4d42b2ee2f2d696425e4f934e7d0dc5add45
davididev <davidwaggener@gmail.com>
2024-12-18 13:00:10 -0500

    Added basic UI to title screen
    TODO: Correct what Mutti and Hannah contributed to computer
    You can cycle through the items but you don't teleport anywhere yet.

---
+++ Mine/Scripts/Title/title_ui.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@export var panels : Array[NodePath];
+@export var scenes : Array[String];
+var lastID = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SetPanel(0);
+	
+func SetPanel(id : int):
+	if id >= panels.size():
+		id = 0;
+	if id < 0:
+		id = panels.size() - 1;
+	lastID = id;
+	
+	for i in range(0, panels.size()):
+		get_node(panels[i]).visible = (id == i);
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_prev_button_pressed() -> void:
+	SetPanel(lastID - 1);
+
+
+func _on_next_button_pressed() -> void:
+	SetPanel(lastID + 1);
