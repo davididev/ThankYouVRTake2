b0880f51f02e25b801e31666234089938a77e0f2
davididev <davidwaggener@gmail.com>
2025-03-05 12:37:57 -0500

    Added paint brush

--- Mine/Scripts/Experiences/mutti_paint_canvas.gd
+++ Mine/Scripts/Experiences/mutti_paint_canvas.gd
@@ -4,8 +4,10 @@ static var CurrentColor : Color;
 var player_body : Node3D = null;
 @export var path_to_renderer : NodePath;
 var currentTexture : Texture2D;
+var lastVertex : int = -1;
 
 func _ready() -> void:
+	lastVertex = -1;
 	CurrentColor = Color.BLACK;
 	currentTexture = load("res://Mine/Models/Mutti/Canvas/StartingWhiteTexture.png");
 	#var datam = currentTexture.get_data();
@@ -25,3 +27,4 @@ func _on_body_entered(body: Node) -> void:
 func _on_body_exited(body: Node) -> void:
 	if body.is_in_group("player_body"):  #Remove from trace
 		player_body = null;
+		lastVertex = -1;
