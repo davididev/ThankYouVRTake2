9c9209a93e50b7bca5b2e5a6f34a6b1e4355ea53
davididev <davidwaggener@gmail.com>
2025-04-07 19:23:35 -0400

    Lightning gun functional

--- Mine/Scripts/Experiences/bullet.gd
+++ Mine/Scripts/Experiences/bullet.gd
@@ -4,15 +4,19 @@ class_name BulletInstance extends Area3D
 @export var DamageAmount = 1;
 @export var DamageType = 0;
 @export var ExplosionPrefab : PackedScene;
-@export var SoundOnSpawn : String = "scifi/Sound.wav"
+@export var SoundOnSpawn : String = "scifi/Sound.wav";
 signal OnBulletDamage(amount : int, type : int);  #Template for things that interact with bullet
+var played_sound = false;
 
 func _ready() -> void:
-	SoundFXPlayer.PlaySound(SoundOnSpawn, get_tree(), global_position, 4.0, 10.0)
 	top_level = true;
+	played_sound = false;
+	
 
 func _process(delta: float) -> void:
-	
+	if played_sound == false:
+		SoundFXPlayer.PlaySound(SoundOnSpawn, get_tree(), global_position, 4.0, 10.0)
+		played_sound = true;
 	var pos = global_position;
 	pos = pos - (basis.z * ForwardVelocity * delta);
 	global_position = pos;
