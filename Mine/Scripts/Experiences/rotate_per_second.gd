76f80552319a3213efb1b559e44f81530e8f799e
davididev <davidwaggener@gmail.com>
2025-04-08 23:55:57 -0400

    Fixed the rotation to use radians instead of degrees

--- Mine/Scripts/Experiences/rotate_per_second.gd
+++ Mine/Scripts/Experiences/rotate_per_second.gd
@@ -2,9 +2,10 @@ class_name RotatePerSecond extends MeshInstance3D
 
 @export var RotateAmount : Vector3;
 @export var Multiplier = 0.0;
+const RAD_TO_DEG = 0.0174533;
 
 func _process(delta: float) -> void:
 	var thisStepRot = RotateAmount * Multiplier * delta;
-	rotate_x(thisStepRot.x);
-	rotate_y(thisStepRot.y);
-	rotate_z(thisStepRot.z);
+	rotate_x(thisStepRot.x * RAD_TO_DEG);
+	rotate_y(thisStepRot.y * RAD_TO_DEG);
+	rotate_z(thisStepRot.z * RAD_TO_DEG);
