067c079a06cd71f7223bba42410336dd40ed6930
davididev <davidwaggener@gmail.com>
2025-06-05 20:51:58 -0400

    Loading of zipline works
    But the zipline itself hasn't been made yet, nor has the scene been finished.

--- Mine/Prefabs/Experiences/Eros/eros_Program.gd
+++ Mine/Prefabs/Experiences/Eros/eros_Program.gd
@@ -1,6 +1,8 @@
 class_name ErosProgram extends Node3D
 
 @export var NodesToReset : Array[NodePath];
+@export var GridMaps : Array[NodePath];
+const LOAD_MOVE_PER_SECOND = 40.0
 signal OnEnableNode();
 
 func SetEnable(b: bool):
@@ -10,3 +12,18 @@ func SetEnable(b: bool):
 		for i in range(0, NodesToReset.size()):
 			get_node(NodesToReset[i]).emit_signal("OnEnableNode");
 	
+	var enabledMask = pow(2, 1-1);
+	var disabledMask = 0;
+	for i in range(0, GridMaps.size()):
+		var gm = get_node(GridMaps[i]) as GridMap;
+		if b == true:
+			gm.collision_layer = enabledMask;
+		else:
+			gm.collision_layer = disabledMask;
+
+
+func _process(delta: float) -> void:
+	if ErosComputer.IsLoading == true:
+		var vec = global_position;
+		vec.y = move_toward(vec.y, 0.0, LOAD_MOVE_PER_SECOND * delta)
+		global_position = vec;
--- Mine/Prefabs/Experiences/Eros/eros_computer.gd
+++ Mine/Prefabs/Experiences/Eros/eros_computer.gd
@@ -15,12 +15,13 @@ func _process(delta: float) -> void:
 	#If Loading is set to true, move the experience up until it hits the target
 	if InitLoad == true:  #First step of loading
 		for i in range(0, Experience_Paths.size()):
-			get_node(Experience_Paths[i]).visible = (i == EricComputerUI.CurrentExperience);
+			get_node(Experience_Paths[i]).call("SetEnable", (i == EricComputerUI.CurrentExperience));
 		IsLoading = true;
 		InitLoad = false;
+		SoundFXPlayer.PlaySound("LoadMatrix.mp3", get_tree(), global_position, 5.0, 20.0);
 		
 	if UnloadNow == true:
 		UnloadNow = false;
 		for i in range(0, Experience_Paths.size()):
-			get_node(Experience_Paths[i]).visible = false;
+			get_node(Experience_Paths[i]).call("SetEnable", false);
 	pass;
