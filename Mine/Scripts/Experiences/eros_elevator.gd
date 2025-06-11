5167a0576ca626aeb138790bbcdfb113e15d8814
davididev <davidwaggener@gmail.com>
2025-06-11 12:46:57 -0400

    Platform is working
    I had to change some things in the player boilerplate and re-define the elevator collision but it carries you now.

--- Mine/Scripts/Experiences/eros_elevator.gd
+++ Mine/Scripts/Experiences/eros_elevator.gd
@@ -22,15 +22,36 @@ func _SetCollisionStatus(isEnabled : bool):
 	for np in Wall_Meshes:
 		var mi = get_node(np) as MeshInstance3D;
 		mi.visible = isEnabled;
+		
+	#Allow movement if the rigidbody is moving
+	axis_lock_linear_x = !isEnabled;
+	axis_lock_linear_y = !isEnabled;
+	axis_lock_linear_z = !isEnabled;
+	
+var reset_timer = 0.0;
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	var moveRel = Vector3.ZERO;
+	var moved = false;
+	if reset_timer > 0.0:
+		reset_timer -= delta;
+		if reset_timer <= 0.0:
+			target_progress = 0.0;
 	if current_progress != target_progress:
 		current_progress = move_toward(current_progress, target_progress, Move_Multiplier * delta);
 		var p = get_node(Follow_Path_Ref) as PathFollow3D;
 		p.progress_ratio = sqrt(current_progress) / sqrt(MAX_PROGRESS);
 		
+		moveRel = p.global_position - global_position;
+		moved = true;
+		#global_position = p.global_position;
+		
 		if is_equal_approx(current_progress, target_progress):
 			_SetCollisionStatus(false);
+			reset_timer = 4.0;
+	#linear_velocity = moveRel;
+	move_and_collide(moveRel);
+
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
@@ -40,5 +61,4 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	await get_tree().create_timer(2.0).timeout;
-	target_progress = 0.0;
+	pass;
