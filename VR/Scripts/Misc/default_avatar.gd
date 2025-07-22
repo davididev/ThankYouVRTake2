e330378d2bcd087bb8afa8f4479a9a70a4fc63ab
davididev <davidwaggener@gmail.com>
2025-07-22 14:39:55 -0400

    Avatar work
    Still trying to get avatar to line up with the eyes
    Hands match the avatar
    Animation tree enabled but not working yet

--- VR/Scripts/Misc/default_avatar.gd
+++ VR/Scripts/Misc/default_avatar.gd
@@ -96,11 +96,11 @@ func _calculate_delta_change(delta):
 	
 	var newPos = get_node(Player_Body_Path).global_position;
 	#newPos.y -= pb._player_height_override_current * 2.0;
-	var cameraOffset = get_node(Player_Body_Path).player_head_height * Vector3.DOWN;
+	#newPos.y += (get_node(Camera_Path).global_position.y - get_node(Player_Body_Path).global_position.y) / 2.0;
 	#eye_midPoint.z *= -1.0;
 	#eye_midPoint.x *= -1.0;
 	#eye_midPoint.y *= -1.0;
-	global_position = newPos + cameraOffset;
+	global_position = newPos;
 	#get_child(0).position = eye_midPoint / scale;  #Use the eye local position to set a child to position
 	#skel.set_bone_pose_rotation(BONE_NECK, get_node(Camera_Path).get_quaternion())
 	
